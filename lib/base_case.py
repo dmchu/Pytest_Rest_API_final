ffd655841c6514aa1e180bbd3193a89109a6e5e2
dmchu <Q3X-o8X-bra-fG2>
2021-07-12 11:11:46 -0400

    add lib folder

---
+++ lib/base_case.py
@@ -0,0 +1,35 @@
+import json.decoder
+from requests import Response
+from datetime import datetime as dt
+
+class BaseCase:
+    def get_cookie(self, response:Response, cookie_name):
+        assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
+        return response.cookies.get(cookie_name)
+
+    def get_header(self, response:Response, headers_name):
+        assert headers_name in response.headers, f"Cannon find header with the name {headers_name} in the last response"
+        return response.headers.get(headers_name)
+
+    def get_json_value(self, response:Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecodeError:
+            assert False, f"Response is not in JSON Format, Response test is '{response.text}'"
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}"
+        return response_as_dict.get(name)
+
+    def prepare_registration_data(self, email=None) -> dict:
+        if email is None:
+            base_part = "learnqa"
+            domain = "example.com"
+            random_part = dt.now().strftime("%m%d%Y%H%M%S")
+            email = f"{base_part}{random_part}@{domain}"
+
+        return {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
