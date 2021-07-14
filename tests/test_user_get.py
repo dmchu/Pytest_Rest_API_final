7ed2ccd10e3b61c35901c586f376bd9516dc2b31
dmchu <Q3X-o8X-bra-fG2>
2021-07-14 09:10:52 -0400

    ex16

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -2,8 +2,8 @@ from lib.base_case import BaseCase
 from lib.assersions import Assertions as AS
 from lib.my_requests import MyRequests as MR
 
-class TestUserGet(BaseCase):
 
+class TestUserGet(BaseCase):
     BASE_URI: str = "/user/"
 
     def test_get_user_details_not_auth(self):
@@ -35,5 +35,33 @@ class TestUserGet(BaseCase):
             'auth_sid': auth_sid
         }
         response2 = MR.get(URI2, headers=headers, cookies=cookies)
-        expected_fields = ["username", "email", "firstName", "lastName"]
+        expected_fields = ["id", "username", "email", "firstName", "lastName"]
         AS.assert_json_has_keys(response2, expected_fields)
+
+    def test_get_user_details_with_auth_as_another_user(self):
+        URI1 = self.BASE_URI + "login"
+        user_email = "vinkotov@example.com"
+        user_password = "1234"
+        data = {
+            'email': user_email,
+            'password': user_password
+        }
+        response1 = MR.post(URI1, data=data)
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response1, "user_id")
+
+        URI2 = self.BASE_URI + str(user_id_from_auth_method - 1)
+        headers = {
+            'x-csrf-token': token
+        }
+        cookies = {
+            'auth_sid': auth_sid
+        }
+        response2 = MR.get(URI2, headers=headers, cookies=cookies)
+
+        expected_fields = ["username"]
+        unexpected_fields = ["id", "email", "firstName", "lastName"]
+
+        AS.assert_json_has_keys(response2, expected_fields)
+        AS.assert_json_has_no_keys(response2, unexpected_fields)
