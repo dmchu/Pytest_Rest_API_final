ecffab65cc6725d3a61f66d77543447a39ea5bd2
dmchu <Q3X-o8X-bra-fG2>
2021-07-15 17:26:57 -0400

    refactor test_user_edit.py

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,49 +1,27 @@
 from lib.base_case import BaseCase
 from lib.assersions import Assertions as AS
 from lib.my_requests import MyRequests as MR
-
+from lib.helpers import Helpers as HP
 
 class TestUserEdit(BaseCase):
     BASE_URI: str = "/user/"
     URI_LOGIN: str = BASE_URI + "login"
 
-
-    def setup(self):
-        # Registration
-        register_data = self.prepare_registration_data()
-        response1 = MR.post(self.BASE_URI, data=register_data)
-        AS.assert_code_status(response1, 200)
-        AS.assert_json_has_key(response1, "id")
-        self.registered_user: dict = {
-            "user_email": register_data.get("email"),
-            "user_password": register_data.get("password"),
-            "user_id": self.get_json_value(response1, "id")
-        }
-
-
     def test_edit_just_created_user(self):
-        user_email = self.registered_user.get("user_email")
-        user_password = self.registered_user.get("user_password")
-        user_id = self.registered_user.get("user_id")
+        registered_user: dict = HP.register_user(HP)
+        user_email = registered_user.get("user_email")
+        user_password = registered_user.get("user_password")
+        user_id = registered_user.get("user_id")
 
-        # Authorization
-        login_data = {
-            'email': user_email,
-            'password': user_password
-        }
-
-        response2 = MR.post(self.URI_LOGIN, data=login_data)
-
-        auth_sid = self.get_cookie(response2, "auth_sid")
-        token = self.get_header(response2, "x-csrf-token")
+        login_user_response = HP.authorize_user(HP, user_email, user_password)
 
         URI_USER = self.BASE_URI + str(user_id)
         new_name = "Changed Name"
         headers = {
-            'x-csrf-token': token
+            'x-csrf-token': login_user_response.get("token")
         }
         cookies = {
-            'auth_sid': auth_sid
+            'auth_sid': login_user_response.get("auth_sid")
         }
         edit_data = {
             'firstName': new_name
@@ -59,7 +37,8 @@ class TestUserEdit(BaseCase):
         AS.assert_json_value_by_name(response4, "firstName", new_name, "Wrong name of user after update")
 
     def test_edit_existing_user_without_authorization(self):
-        user_id = self.registered_user.get("user_id")
+        registered_user: dict = HP.register_user(HP)
+        user_id = registered_user.get("user_id")
         URI_USER = self.BASE_URI + str(user_id)
         new_name = "Changed Name2"
         headers = {
@@ -78,11 +57,14 @@ class TestUserEdit(BaseCase):
         AS.assert_response_text(response, "Auth token not supplied")
 
     def test_edit_existing_user_with_authorization_by_another_user(self):
+        registered_user: dict = HP.register_user(HP)
+        correct_user_email = registered_user.get("user_email")
+        correct_user_password = registered_user.get("user_password")
+        user_id = registered_user.get("user_id")
+
         user_email = "vinkotov@example.com"
         user_password = "1234"
-        correct_user_email = self.registered_user.get("user_email")
-        correct_user_password = self.registered_user.get("user_password")
-        user_id = self.registered_user.get("user_id")
+
 
         # Authorization with another user
         login_data = {
@@ -112,23 +94,16 @@ class TestUserEdit(BaseCase):
         AS.assert_code_status(response2, 400)
 
         # Authorization with correct user
-        login_data_2 = {
-            'email': correct_user_email,
-            'password': correct_user_password
-        }
+        login_user_response = HP.authorize_user(HP, correct_user_email, correct_user_password)
 
-        response3 = MR.post(self.URI_LOGIN, data=login_data_2)
-
-        auth_sid_2 = self.get_cookie(response3, "auth_sid")
-        token_2 = self.get_header(response3, "x-csrf-token")
-
+        URI_USER = self.BASE_URI + str(user_id)
+        new_name = "Changed Name"
         headers_2 = {
-            'x-csrf-token': token_2
+            'x-csrf-token': login_user_response.get("token")
         }
         cookies_2 = {
-            'auth_sid': auth_sid_2
+            'auth_sid': login_user_response.get("auth_sid")
         }
-
         # Get user data and verify that changes was not made
 
         response4 = MR.get(URI_USER, headers=headers_2, cookies=cookies_2)
@@ -138,29 +113,21 @@ class TestUserEdit(BaseCase):
             "First name should not be changed by user with another authenticated user, but it did"
 
     def test_edit_user_email_with_wrong_format(self):
-        user_email = self.registered_user.get("user_email")
-        user_password = self.registered_user.get("user_password")
-        user_id = self.registered_user.get("user_id")
+        registered_user: dict = HP.register_user(HP)
+        user_email = registered_user.get("user_email")
+        user_password = registered_user.get("user_password")
+        user_id = registered_user.get("user_id")
 
-        # Authorization
-        login_data = {
-            'email': user_email,
-            'password': user_password
-        }
-
-        response2 = MR.post(self.URI_LOGIN, data=login_data)
-
-        auth_sid = self.get_cookie(response2, "auth_sid")
-        token = self.get_header(response2, "x-csrf-token")
+        login_user_response = HP.authorize_user(HP, user_email, user_password)
 
         URI_USER = self.BASE_URI + str(user_id)
         new_email = user_email.replace("@", ".")
 
         headers = {
-            'x-csrf-token': token
+            'x-csrf-token': login_user_response.get("token")
         }
         cookies = {
-            'auth_sid': auth_sid
+            'auth_sid': login_user_response.get("auth_sid")
         }
         edit_data = {
             'email': new_email
@@ -180,31 +147,24 @@ class TestUserEdit(BaseCase):
             "Email should not be changed by user to email with wrong format, but it did"
 
     def test_edit_user_first_name_with_one_character(self):
-        user_email = self.registered_user.get("user_email")
-        user_password = self.registered_user.get("user_password")
-        user_id = self.registered_user.get("user_id")
+        registered_user: dict = HP.register_user(HP)
+        user_email = registered_user.get("user_email")
+        user_password = registered_user.get("user_password")
+        user_id = registered_user.get("user_id")
 
-        # Authorization
-        login_data = {
-            'email': user_email,
-            'password': user_password
-        }
-
-        response2 = MR.post(self.URI_LOGIN, data=login_data)
-
-        auth_sid = self.get_cookie(response2, "auth_sid")
-        token = self.get_header(response2, "x-csrf-token")
+        login_user_response = HP.authorize_user(HP, user_email, user_password)
 
         URI_USER = self.BASE_URI + str(user_id)
-        new_name = "V"
+        new_email = user_email.replace("@", ".")
+
         headers = {
-            'x-csrf-token': token
+            'x-csrf-token': login_user_response.get("token")
         }
         cookies = {
-            'auth_sid': auth_sid
+            'auth_sid': login_user_response.get("auth_sid")
         }
         edit_data = {
-            'firstName': new_name
+            'firstName': "V"
         }
         # Edit user data
 
@@ -218,5 +178,5 @@ class TestUserEdit(BaseCase):
         response4 = MR.get(URI_USER, headers=headers, cookies=cookies)
         response_data = response4.json()
         user_first_name = response_data.get("firstName")
-        assert user_first_name != new_name, \
+        assert user_first_name != "V", \
             "First name should not be changed by user to very short name, but it did"
