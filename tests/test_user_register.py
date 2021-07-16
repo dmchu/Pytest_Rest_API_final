4be6a1aa4115488c74d1172fb871a17c407bf0d9
dmchu <Q3X-o8X-bra-fG2>
2021-07-16 12:59:48 -0400

    refactor allure reporting for test_user_register.py

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,23 +1,30 @@
 import pytest
+import allure
 
 from lib.base_case import BaseCase
 from lib.assersions import Assertions as AS
 from lib.my_requests import MyRequests as MR
 from datetime import datetime as dt
 
+
+@allure.epic("User Registration cases")
 class TestUserRegister(BaseCase):
 
     URI = "/user/"
     params = [('firstName'), ('lastName'), ('username'), ('password'), ('email')]
 
-
+    @allure.feature("User Registration")
+    @allure.story("positive - Register new user")
+    @allure.description("Verifiying that new user can be registered")
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
         response = MR.post(self.URI, data=data)
         AS.assert_code_status(response, 200)
         AS.assert_json_has_key(response, "id")
 
-
+    @allure.feature("User Registration")
+    @allure.story("negative - Register user with already existing 'email'")
+    @allure.description("Verifiying that new user can not be registered with existing 'email'")
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
         data = self.prepare_registration_data(email)
@@ -26,6 +33,9 @@ class TestUserRegister(BaseCase):
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists",\
             f"Unexpected response content {response.content}"
 
+    @allure.feature("User Registration")
+    @allure.story("negative - Register user with wrong format 'email'")
+    @allure.description("Verifiying that new user can not be registered with wrong format 'email'")
     def test_create_user_with_wrong_email(self):
         email = 'vinkotov.example.com'
         data = self.prepare_registration_data(email)
@@ -34,6 +44,9 @@ class TestUserRegister(BaseCase):
         assert response.content.decode("utf-8") == f"Invalid email format", \
             f"Unexpected response content {response.content}"
 
+    @allure.feature("User Registration")
+    @allure.story("negative - Register user with missing 'data parameter'")
+    @allure.description("Verifiying that new user can not be registered with missing 'data parameter'")
     @pytest.mark.parametrize("data_parameter", params)
     def test_create_user_with_missing_data_parameter(self, data_parameter):
         data = self.prepare_registration_data(missing_param=data_parameter)
@@ -42,7 +55,9 @@ class TestUserRegister(BaseCase):
         assert response.content.decode("utf-8") == f"The following required params are missed: {data_parameter}", \
             f"Unexpected response content {response.content}"
 
-
+    @allure.feature("User Registration")
+    @allure.story("negative - Register user with empty 'data parameter'")
+    @allure.description("Verifiying that new user can not be registered with empty 'data parameter'")
     @pytest.mark.parametrize("data_parameter", params)
     def test_create_user_with_empty_data_parameter(self, data_parameter):
         data = self.prepare_registration_data(empty_param=data_parameter)
@@ -51,6 +66,9 @@ class TestUserRegister(BaseCase):
         assert response.content.decode("utf-8") == f"The value of '{data_parameter}' field is too short", \
             f"Unexpected response content {response.content}"
 
+    @allure.feature("User Registration")
+    @allure.story("negative - Register user with short 'first name'")
+    @allure.description("Verifiying that new user can not be registered with short 'first name'")
     def test_create_user_with_short_name(self):
         email = 'v'
         data = self.prepare_registration_data(email)
@@ -59,6 +77,9 @@ class TestUserRegister(BaseCase):
         assert response.content.decode("utf-8") == f"The value of 'email' field is too short", \
             f"Unexpected response content {response.content}"
 
+    @allure.feature("User Registration")
+    @allure.story("negative - Register user with very long 'first name'")
+    @allure.description("Verifiying that new user can not be registered with very long 'first name'")
     def test_create_user_with_long_name(self):
         base_part = "learnqa" * 40
         domain = "example.com"
